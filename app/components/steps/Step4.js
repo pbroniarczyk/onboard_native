9d6bb82d4aedb7fade3b5051903808b94f0333c3
pawel <pbroniarczyk@outlook.com>
2018-05-28 10:21:53 +0200

    skill view added (adding skills with no saving & some bugs)

--- app/components/steps/Step4.js
+++ app/components/steps/Step4.js
@@ -1,22 +1,156 @@
-import React from 'react';
-import { Text, View, Button } from 'react-native';
+import React, { Component } from 'react';
+import { Animated, Text, View, Button, TextInput } from 'react-native';
+
+// Components
+import ButtonWrapper from "../common/ButtonWrapper";
 
 // Assets
 import { styles } from "./stepsStyles";
 
 
-export default class Step4 extends React.Component {
-	render() {
+const skills = [
+    'JavaScript', 'CSS3', 'HTML', 'React', 'Python',
+    'Sass', 'SQL', 'GraphQL', 'C++', 'Ruby', 'Redux',
+    'Java', 'Bootstrap', 'Foundation', 'Flask',
+    'TypeScript', 'Angular', 'Vue', 'NodeJS', 'Git',
+    'Webpack', 'Gulp', 'Grunt'
+];
+const skillCloud = [...skills];
+
+export default class Step4 extends Component { 
+    constructor() {
+        super();
+
+        this.state = {
+            fadeAnim: new Animated.Value(0),
+            disabled: true,
+            inputDisabled: false,
+            skillCounter: 0,
+            searchedSkill: "",
+            skillArray: [] ,
+            skillPlaceholder: [1, 2, 3, 4, 5, 6]
+        }
+    }
+
+    checkDisabled = () => {
+        console.log("CHECK DISABLED")
+        const skillCover = document.querySelector('.skill-cloud');
+        if (this.state.skillCounter === 6 && !skillCover.classList.contains('skill-cloud--disabled')) {
+            this.setState({
+                disabled: false,
+                inputDisabled: true
+            });
+            skillCover.classList.add('skill-cloud--disabled');
+        } else if (this.state.skillCounter !== 6 || skillCover.classList.contains('skill-cloud--disabled')) {
+            this.setState({
+                disabled: true,
+                inputDisabled: false
+            });
+            skillCover.classList.remove('skill-cloud--disabled');
+        }
+    }
+
+    selectSkill = (element, index) => {
+        console.log("SELECT SKILL", element, index)
+        const skillArray = [...this.state.skillArray];
+        skillArray.push(element);
+        this.setState({
+            skillCounter: this.state.skillCounter + 1,
+            skillArray
+        });
+
+        // let skillSelected = element,
+        //     emptySkill = document.querySelectorAll('.skill--empty');
+        // classHandler(emptySkill[0], skillSelected);
+        // skillCloud.splice(index, 1)
+    }
+
+    removeSkill = event => {
+        console.log("REMOVE SKILL")
+        this.setState(
+            {skillCounter: this.state.skillCounter - 1},
+            // () => this.checkDisabled()
+        );
+
+        let skillFull = event.currentTarget;
+        // classHandler(skillFull, '');
+    }
+
+    componentDidMount = () => {
+        Animated.timing(
+            this.state.fadeAnim,
+            {
+                toValue: 1,
+                duration: 400,
+            }
+        ).start();
+    }
+
+    componentWillUnmount = () => {
+        this.props.userProfileUpdate(workObj);
+        Animated.timing(
+            this.state.fadeAnim,
+            {
+                toValue: 0,
+                duration: 400,
+            }
+        ).start(); 
+	}
+    
+    render() {
+        console.log("skill state", this.state)
+        
 		return (
 			<View style={styles.step}>
-				<Text style={styles.text}>{this.props.step}</Text>
-                <Button 
-                    title="Next"
-                    onPress={this.props.nextStep}
-                />
-                <Button 
-                    title="Previous"
-                    onPress={this.props.prevStep}
+				<Text style={styles.headline}>Umiejętności</Text>
+                <Text style={styles.text}>Wyszukaj lub wybierz z listy najpopularniejszych główne umiejętności, które posiadasz.</Text>
+                
+                <View>
+                    <View style={styles.autocomplete_wrapper}>
+                        <TextInput
+                            name="skill"
+                            placeholder="Nazwa umiejętności"
+                            style={styles.input}
+                            onChangeText={(text) => this.setState({searchedSkill: text})}
+                            value={this.state.searchedSkill} />
+                        <Button
+                            title="Dodaj"
+                            style={styles.autocomplete_btn}
+                            onPress={this.props.prevStep} />
+                    </View>
+
+                    <View style={styles.skill_cloud}>
+                        {skillCloud.map((e,i) => <Text key={i} style={styles.skillcloud_item} onPress={this.selectSkill.bind(this, e, i)}>{e}</Text>)}
+                    </View>
+
+                    <View className="skill-counter">
+                        <Text>Twoje umiejętności</Text>
+                        <Text><Text className="skill-count">{this.state.skillCounter}</Text>/6</Text>
+                    </View>
+
+                    <View style={styles.skill_wrapper}> 
+                        {
+                            this.state.skillPlaceholder.map((skill, index) => 
+                                <View
+                                    key={index}
+                                    onPress={() => this.removeSkill}
+                                    style={
+                                        this.state.skillArray[index] 
+                                        ? styles.skill_full 
+                                        : styles.skill} >
+
+                                    <Text style={styles.addedSkill}>
+                                        {this.state.skillArray[index]}
+                                    </Text>
+                                </View>
+                            )
+                        }
+                    </View>
+
+                </View>
+                <ButtonWrapper 
+                    nextStep={this.props.nextStep}
+                    prevStep={this.props.prevStep}
                 />
 			</View>
 		);
--- app/components/steps/stepsStyles.js
+++ app/components/steps/stepsStyles.js
@@ -4,6 +4,7 @@ export const styles = StyleSheet.create({
 	stepContainer: {
 		flex: 1
 	},
+
 	step: {
 		backgroundColor: "#FFFFFF",
 		flex: 1,
@@ -11,17 +12,21 @@ export const styles = StyleSheet.create({
 		justifyContent: "space-between",
 		padding: 20
 	},
+	
 	row: {
 		flexDirection: "row"
 	},
+	
 	headline: {
 		color: "black",
 		fontSize: 23,
 		fontWeight: "bold"
 	},
-    text: { 
+	
+	text: { 
         color: "#95989a"
 	},
+	
 	input: {
 		width: 300
 	},
@@ -56,5 +61,75 @@ export const styles = StyleSheet.create({
 		marginBottom: 10,
 		borderWidth: 1,
 		borderColor: "#0388d1",
+	},
+
+	// Step4 - skills
+	autocomplete_wrapper: {
+		display: "flex"
+	},
+	
+	autocomplete_btn: {
+		width: 70
+	},
+
+	skill_cloud: {
+		flexWrap: "wrap",
+		marginBottom: 30,
+		width: 300,
+		minHeight: 65,
+		maxHeight: 90,
+		overflow: "hidden",
+		position: "relative"
+	},
+	
+	skillcloud_item: {
+		fontSize: 12,
+		display: "flex",
+		borderColor: "#E5E5E5",
+		borderRadius: 3,
+		padding: 5,
+		color: "#404040",
+		marginRight: 5,
+		marginBottom: 6,
+	},
+	
+	skill_counter: {
+		display: "flex",
+		justifyContent: "space-between",
+		flexWrap: "wrap",
+	},
+	
+	skill_wrapper: {
+		display: "flex",
+		flexWrap: "wrap",
+	},
+	
+	skill: {
+		backgroundColor: "#E5E5E5",
+		width: 120,
+		display: "flex",
+		height: 28,
+		marginBottom: 7,
+		marginRight: 7,
+		borderColor: "#E5E5E5",
+		borderRadius: 3,
+	},
+	
+	skill_full: {
+		width: 120,
+		display: "flex",
+		alignItems: "center",
+		justifyContent: "center",
+		height: 28,
+		marginBottom: 7,
+		marginRight: 7,
+		borderRadius: 3,
+
+		backgroundColor: "#0388d1",
+		borderColor: "#0388d1",
+	},
+
+	addedSkill: {
+		color: "#FFFFFF"
 	}
-});
+})
